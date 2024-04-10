5b68bd1fc82db98f7ad7d3a18c1b1d080395ab45
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-04-10 01:29:33 -0400

    feat: switch to flakes This will make keeping things up-to-date much easier, since we can leverage the power of flakes to ensure our glfw source remains updated

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    glfw = {
+      url = "github:glfw/glfw";
+      flake = false;
+    };
+  };
+
+  outputs = { self, ... } @ inputs:
+  let
+    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+    inherit (pkgs) stdenv;
+  in {
+    packages."x86_64-linux".default = stdenv.mkDerivation {
+      name = "glfw-libs";
+
+      src = inputs.glfw;
+
+      buildInputs = with pkgs; [
+        cmake
+        gcc pkgsCross.mingwW64.buildPackages.gcc
+        pkg-config
+        libffi
+
+        libGL
+
+        # Wayland
+        wayland
+        wayland-protocols
+        libxkbcommon
+
+        # X11
+        xorg.libX11
+        xorg.libXrandr
+        xorg.libXinerama
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXext
+      ];
+
+      dontUseCmakeConfigure = true;
+
+      buildPhase = ''
+      cmake -S $src -B linux_build -D BUILD_SHARED_LIBS=ON
+      cmake --build linux_build
+
+      cmake -S $src -B windows_build -D BUILD_SHARED_LIBS=ON -D CMAKE_TOOLCHAIN_FILE=CMake/x86_64-w64-mingw32.cmake
+      cmake --build windows_build
+      '';
+
+      installPhase = ''
+      mkdir -p $out
+
+      cp $(readlink -f linux_build/src/libglfw.so) $out/libglfw.so
+      cp windows_build/src/glfw3.dll $out/glfw3.dll
+      '';
+    };
+  };
+}
