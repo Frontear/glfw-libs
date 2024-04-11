9a61777defa69c545559fddb3ad840aeb6a3e059
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-04-11 01:38:45 -0400

    feat: better flake handling + pkgs.callPackage these are better and more standard ways of doing things

--- flake.nix
+++ flake.nix
@@ -10,52 +10,19 @@
 
   outputs = { self, ... } @ inputs:
   let
-    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-    inherit (pkgs) stdenv;
+    # https://ayats.org/blog/no-flake-utils
+    eachSystem = function: inputs.nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ] (system: function (import inputs.nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+    }));
   in {
-    packages."x86_64-linux".default = stdenv.mkDerivation {
-      name = "glfw-libs";
-
-      src = inputs.glfw;
-
-      buildInputs = with pkgs; [
-        cmake
-        gcc pkgsCross.mingwW64.buildPackages.gcc
-        pkg-config
-        libffi
-
-        libGL
-
-        # Wayland
-        wayland
-        wayland-protocols
-        libxkbcommon
-
-        # X11
-        xorg.libX11
-        xorg.libXrandr
-        xorg.libXinerama
-        xorg.libXcursor
-        xorg.libXi
-        xorg.libXext
-      ];
-
-      dontUseCmakeConfigure = true;
-
-      buildPhase = ''
-      cmake -S $src -B linux_build -D BUILD_SHARED_LIBS=ON
-      cmake --build linux_build
-
-      cmake -S $src -B windows_build -D BUILD_SHARED_LIBS=ON -D CMAKE_TOOLCHAIN_FILE=CMake/x86_64-w64-mingw32.cmake
-      cmake --build windows_build
-      '';
-
-      installPhase = ''
-      mkdir -p $out
-
-      cp $(readlink -f linux_build/src/libglfw.so) $out/libglfw.so
-      cp windows_build/src/glfw3.dll $out/glfw3.dll
-      '';
-    };
+    packages = eachSystem (pkgs: {
+      default = pkgs.callPackage ./glfw.nix { inherit (inputs) glfw; };
+    });
   };
 }
---
+++ glfw.nix
@@ -0,0 +1,44 @@
+{ pkgs, stdenv, glfw }: stdenv.mkDerivation {
+  name = "glfw-libs";
+
+  src = glfw;
+
+  buildInputs = with pkgs; [
+    cmake
+    gcc pkgsCross.mingwW64.buildPackages.gcc
+    pkg-config
+    libffi
+
+    libGL
+
+    # Wayland
+    wayland
+    wayland-protocols
+    libxkbcommon
+
+    # X11
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXinerama
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXext
+  ];
+
+  dontUseCmakeConfigure = true;
+
+  buildPhase = ''
+  cmake -S $src -B linux_build -D BUILD_SHARED_LIBS=ON
+  cmake --build linux_build
+
+  cmake -S $src -B windows_build -D BUILD_SHARED_LIBS=ON -D CMAKE_TOOLCHAIN_FILE=CMake/x86_64-w64-mingw32.cmake
+  cmake --build windows_build
+  '';
+
+  installPhase = ''
+  mkdir -p $out
+
+  cp $(readlink -f linux_build/src/libglfw.so) $out/libglfw.so
+  cp windows_build/src/glfw3.dll $out/glfw3.dll
+  '';
+}
