8b2b59f09afa51d148ea2e5a5f14e2d12342bc9e
Ali Rizvi <perm-iterate-0b@icloud.com>
2024-06-28 15:00:02 -0400

    feat: organize package and flake I'm well aware the build outputs of this package are not up to nixpkgs standards, or even really nix standards. However, these packages aren't intended to be run on NixOS, or linked through Nix, so I don't really care.

--- flake.nix
+++ flake.nix
@@ -1,71 +1,18 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  description = "Description for the project";
 
-    glfw = {
-      url = "github:glfw/glfw";
-      flake = false;
-    };
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    glfw.url = "github:glfw/glfw";
+    glfw.flake = false;
   };
 
-  outputs = { self, ... } @ inputs:
-  let
-    # https://ayats.org/blog/no-flake-utils
-    eachSystem = function: inputs.nixpkgs.lib.genAttrs [
-      "x86_64-linux"
-      "x86_64-darwin"
-      "aarch64-linux"
-      "aarch64-darwin"
-    ] (system: function (import inputs.nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    }));
-  in {
-    packages = eachSystem (pkgs: {
-      default = pkgs.callPackage ({ pkgs, stdenv }: stdenv.mkDerivation {
-        name = "glfw-libs";
-
-        src = inputs.glfw;
-
-        buildInputs = with pkgs; [
-          cmake
-          gcc pkgsCross.mingwW64.buildPackages.gcc
-          pkg-config
-          libffi
-
-          libGL
-
-          # Wayland
-          wayland
-          wayland-protocols
-          libxkbcommon
-
-          # X11
-          xorg.libX11
-          xorg.libXrandr
-          xorg.libXinerama
-          xorg.libXcursor
-          xorg.libXi
-          xorg.libXext
-        ];
-
-        dontUseCmakeConfigure = true;
-
-        buildPhase = ''
-        cmake -S $src -B linux_build -D BUILD_SHARED_LIBS=ON
-        cmake --build linux_build
-
-        cmake -S $src -B windows_build -D BUILD_SHARED_LIBS=ON -D CMAKE_TOOLCHAIN_FILE=CMake/x86_64-w64-mingw32.cmake
-        cmake --build windows_build
-        '';
-
-        installPhase = ''
-        mkdir -p $out
-
-        cp $(readlink -f linux_build/src/libglfw.so) $out/libglfw.so
-        cp windows_build/src/glfw3.dll $out/glfw3.dll
-        '';
-      }) {};
-    });
+  outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+    perSystem = { config, self', inputs', pkgs, system, ... }: {
+      packages.default = pkgs.callPackage ./glfw { inherit (inputs) glfw; };
+    };
   };
 }
